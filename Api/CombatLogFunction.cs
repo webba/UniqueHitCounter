879479871074c701b070deaec07a5682b41c885d
Anders Urban <minikaliffen@gmail.com>
2021-09-25 03:18:59 +0200

    Things works, stuff do things

--- Api/CombatLogFunction.cs
+++ Api/CombatLogFunction.cs
@@ -10,6 +10,7 @@ using BlazorApp.Shared;
 using System.Text.Json;
 using UniqueHitCounter.Logic;
 using Microsoft.Azure.Storage.Blob;
+using System.Collections.Generic;
 
 namespace BlazorApp.Api
 {
@@ -17,7 +18,7 @@ namespace BlazorApp.Api
     {
         [FunctionName("CombatLogFunction")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "CombatLog")] HttpRequest req,
             [Blob("%FullDataName%", FileAccess.Write)] CloudBlobContainer fullDataBlob,
             [Blob("%ResultsDataName%", FileAccess.Write)] CloudBlobContainer resultsDataBlob,
             ILogger log)
@@ -26,16 +27,74 @@ namespace BlazorApp.Api
             string body = await req.ReadAsStringAsync();
             var combatPost = JsonSerializer.Deserialize<CombatPost>(body, new JsonSerializerOptions
             {
-                PropertyNameCaseInsensitive = true 
+                PropertyNameCaseInsensitive = true
             });
 
-            var result = LogParser.ParseCombatLog(combatPost);
+            IEnumerable<CombatEntry> result = LogParser.ParseCombatLog(combatPost);
 
             var blobName = Guid.NewGuid().ToString();
-            var fullBlockBlob = fullDataBlob.GetBlockBlobReference(blobName);
-            await fullBlockBlob.UploadTextAsync(JsonSerializer.Serialize(result));
+            CombatResults combatResults = new CombatResults { 
+                Created = DateTime.Now,
+                Creator = combatPost.Name,
+                FightStart = DateTime.Now,
+                FightEnd = DateTime.Now,
+                CombatResultsList = new List<CombatResult>() 
+                { 
+                    new CombatResult { 
+                        Player = "test",
+                        BiggestHit = "test",
+                        Hits = 10,
+                        KnocksOver = 10,
+                        TimesHit = 10,
+                        TimesStunned = 10,
+                        TimesThrown = 10
+                    } 
+                } 
+            };
+
+            await SaveFile(fullDataBlob, JsonSerializer.Serialize(result), blobName);
+            await SaveFile(resultsDataBlob, JsonSerializer.Serialize(combatResults), blobName);
 
             return new OkObjectResult(blobName);
+        }
+
+        private static async Task SaveFile(CloudBlobContainer fullDataBlob, string result, string blobName)
+        {
+            var fullBlockBlob = fullDataBlob.GetBlockBlobReference(blobName);
+            fullBlockBlob.Properties.ContentType = "text/json";
+            await fullBlockBlob.UploadTextAsync(result);
+        }
+
+        [FunctionName("CombatLogGetFunction")]
+        public static async Task<IActionResult> CombatLogGetFunction(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "CombatLog/{blobName}")] HttpRequest req,
+            [Blob("%FullDataName%", FileAccess.Read)] CloudBlobContainer fullDataBlob,
+            ILogger log, string blobName)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var fullBlockBlob = fullDataBlob.GetBlockBlobReference(blobName);
+            string v = await fullBlockBlob.DownloadTextAsync();
+
+            IEnumerable <CombatEntry> combatEntry = JsonSerializer.Deserialize<IEnumerable<CombatEntry>>(v, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            return new OkObjectResult(combatEntry);
+        }
+
+        [FunctionName("CombatResultsGetFunction")]
+        public static async Task<IActionResult> CombatResultsGetFunction(
+          [HttpTrigger(AuthorizationLevel.Function, "get", Route = "CombatResults/{blobName}")] HttpRequest req,
+          [Blob("%ResultsDataName%", FileAccess.Read)] CloudBlobContainer fullDataBlob,
+          ILogger log, string blobName)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var fullBlockBlob = fullDataBlob.GetBlockBlobReference(blobName);
+            string v = await fullBlockBlob.DownloadTextAsync();
+
+            CombatResults combatEntry = JsonSerializer.Deserialize<CombatResults>(v, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            return new OkObjectResult(combatEntry);
         }
     }
 }
---
+++ Shared/CombatResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Shared
+{
+    public class CombatResult
+    {
+        public string Player { get; set; }
+        public int Hits { get; set; }
+        public int TimesHit { get; set; }
+        public int TimesStunned { get; set; }
+        public int TimesThrown { get; set; }
+        public int KnocksOver { get; set; }
+        public string BiggestHit { get; set; }
+    }
+}
